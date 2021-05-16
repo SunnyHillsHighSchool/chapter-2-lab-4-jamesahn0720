52e03ed88f6638d7222e5e97dbceeaffd3578475
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-16 10:06:06 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,6 +10,27 @@ public class IteratorRemoverRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases	
+		
+                IteratorRemover o1 = new IteratorRemover("a b c a b c a","a");
+            
+                o1.remove();
+               
+                System.out.println(o1);
+                
+                
+              
+                IteratorRemover o2 = new IteratorRemover("a b c d e f g h i j x x x x","x");
+              
+                o2.remove();
+              
+                System.out.println(o2);
+                
+                
+             
+                IteratorRemover o3 = new IteratorRemover("1 2 3 4 5 6 a b c a b c","b");
+             
+                o3.remove();
+               
+                System.out.println(o3);	
 	}
 }
